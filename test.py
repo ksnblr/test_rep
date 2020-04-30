0d37218b3b92055b30726a5db603da78faa17ca3
Saravanan Kandaswamy <saravanan.kandaswamy@3ds.com>
2020-04-30 13:48:07 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+def about_me(your_name):
+    print("The wise {} loves Python.".format(your_name))
+
+def HW():
+    print("Hello World!")
+
+def Both():
+    HW()
+    about_me("Saravanan")
+
+Both()
+F  = open(r"D:\\temp\\a.txt", "a")
+F.write("ok\n")
